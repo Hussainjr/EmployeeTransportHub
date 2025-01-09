8776f9fd2337f56028eb6e286cc6dcb401eb74db
Hussainjr <syedhussainyjr@gmail.com>
2025-01-10 03:54:23 +0530

    added @SpringBootTest with @Test.

---
+++ src/main/java/com/crm/crm/controller/CrmApplicationTest.java
@@ -0,0 +1,20 @@
+package com.crm.crm.controller;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CrmApplicationTest {
+
+    @Test
+    public void test1(){
+        System.out.println("i am test1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("i am test2");
+    }
+
+
+}
