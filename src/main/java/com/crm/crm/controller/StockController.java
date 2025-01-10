e2392744249530bc3d8f3b293d64407a80e21304
hussainjr <syedhussainyjr@gmail.com>
2025-01-10 16:16:14 +0530

    "all the StockController is updated"

--- src/main/java/com/crm/crm/controller/StockController.java
+++ src/main/java/com/crm/crm/controller/StockController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/v1/stockController")
 public class StockController {
 
     @Autowired private StockService stockService;
